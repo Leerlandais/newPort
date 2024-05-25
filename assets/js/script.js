22b7eac309bd538061b5662593ebe28e9490cf59
leerlandais <leerlandais@gmail.com>
2024-05-25 12:35:41 +0200

    JS written dynamically : OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,15 +25,9 @@ $(document).ready(function() {
 
  function makeGlobalText(datas) {
     for (let data in datas) {
-        if (datas[data].selector === "homepage_h1") $("#homeH1").text(datas[data].theText);
-        if (datas[data].selector === "navHome") $("#navHome").text(datas[data].theText);
-        if (datas[data].selector === "navContact") $("#navContact").text(datas[data].theText);
-        if (datas[data].selector === "navLogin") $("#navLogin").text(datas[data].theText);
-        if (datas[data].selector === "navLogout") $("#navLogout").text(datas[data].theText);
-        if (datas[data].selector === "navAbout") $("#navAbout").text(datas[data].theText);
-        if (datas[data].selector === "logLabelName") $("#logLabelName").text(datas[data].theText);
-        if (datas[data].selector === "logLabelPwd") $("#logLabelPwd").text(datas[data].theText);
-        if (datas[data].selector === "submitLogin") $("#submitLogin").text(datas[data].theText);
+        let selector = datas[data].selector;
+        let theText = datas[data].theText;
+        $(`#${selector}`).text(theText);
     }
  }
 
